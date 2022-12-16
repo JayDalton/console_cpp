01c30e5f87bf680d9bfa3a1f7929765b567a9bf5
JayDalton <github@t-jay.net>
2022-12-16 14:12:35 +0100

    add cmdline parser

--- main.cpp
+++ main.cpp
@@ -10,71 +10,95 @@
 // cmake --build build32 --config Release
 // cmake --build build64 --config Release
 
+// #include <dcmtk/config/osconfig.h>    /* make sure OS specific configuration is included first */
+// #include <dcmtk/dcmdata/dcfilefo.h>
+// #include <dcmtk/dcmdata/dcmetinf.h>
+// #include <dcmtk/dcmimgle/dcmimage.h>
+
 #include <opencv2/opencv.hpp>
 #include <opencv2/core.hpp>
 #include <opencv2/imgcodecs.hpp>
 #include <opencv2/highgui.hpp>
 
-namespace acme
+
+auto main(int argc, char const * const argv[]) -> int
 {
-    auto test(std::span<char const * const> args) -> std::optional<std::string>;
+   const cv::String keys = 
+      "{help h usage ? |      | print this message   }"
+      "{@image1        |      | image1 for compare   }"
+      "{@image2        |<none>| image2 for compare   }"
+      "{@repeat        |1     | number               }"
+      "{path           |.     | path to file         }"
+      "{fps            | -1.0 | fps for output video }"
+      "{N count        |100   | count of objects     }"
+      "{ts timestamp   |      | use time stamp       }"
+   ;
 
-}
+   cv::CommandLineParser parser(argc, argv, keys);
+   parser.about("Application name v1.0.0");
+
+   if (parser.has("help"))
+   {
+      parser.printMessage();
+      return 0;
+   }
+
+   int N = parser.get<int>("N");
+   double fps = parser.get<double>("fps");
+   String path = parser.get<String>("path");
+   auto use_time_stamp = parser.has("timestamp");
+   String img1 = parser.get<String>(0);
+   String img2 = parser.get<String>(1);
+   int repeat = parser.get<int>(2);
+
+   if (!parser.check())
+   {
+      parser.printErrors();
+      return 0;
+   }
+
+   print("Image: {}\n", img1);
+
+   // DcmFileFormat format;
+   // DcmDataset dataset;
+
+   // auto const args{std::span{argv, argv + argc}};
+
+   // if (auto res = acme::test(args))
+   // {
+   //     print("{}\n", res.value());
+   // }
+
+   std::string image_path = "PE_Image.jpg";
+   std::ifstream file(image_path);
+   if (file.is_open())
+   {
+      print("is open\n");
+   }
+   else 
+   {
+      print("is not\n");
+   }
 
 
-auto main(int argc, char const * const argv) -> int
-{
-    print("Console C++\n");
+   cv::Mat img = cv::imread(image_path, cv::IMREAD_COLOR);
 
-    // auto const args{std::span{argv, argv + argc}};
+   if (img.empty())
+   {
+      print("Could not read the image: {}", image_path);
+      return 1;
+   }
 
-    // if (auto res = acme::test(args))
-    // {
-    //     print("{}\n", res.value());
-    // }
+   print("...\n");
 
-    std::string image_path = "PE_Image.jpg";
-    std::ifstream file(image_path);
-    if (file.is_open())
-    {
-        print("is open\n");
-    }
-    else 
-    {
-        print("is not\n");
-    }
+   cv::imshow("Display window", img);
 
+   int k = cv::waitKey(0); // Wait for a keystroke in the window
+   // if (k == 's')
+   // {
+   //     cv::imwrite("starry_night.png", img);
+   // }
 
-    cv::Mat img = cv::imread(image_path, cv::IMREAD_COLOR);
-
-    // std::vector<std::int16_t> buffer;
-    // std::ifstream file("../wordstop.file");
-    // if (file.is_open())
-    // {
-    //     std::copy(
-    //         std::istream_iterator<std::int16_t>{file},
-    //         std::istream_iterator<std::int16_t>(),
-    //         std::inserter(buffer, buffer.begin())
-    //     );
-    // }
-
-
-    if (img.empty())
-    {
-        print("Could not read the image: {}", image_path);
-        return 1;
-    }
-
-    print("...\n");
-
-    cv::imshow("Display window", img);
-
-    int k = cv::waitKey(0); // Wait for a keystroke in the window
-    // if (k == 's')
-    // {
-    //     cv::imwrite("starry_night.png", img);
-    // }
-
-    return 0;
+   return 0;
 }
 
