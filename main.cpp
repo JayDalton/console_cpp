052ae6c80dee68f8d9b1bb95e5112325020d84b5
JayDalton <github@t-jay.net>
2022-12-18 19:48:01 +0100

    try compile dcmtk

--- main.cpp
+++ main.cpp
@@ -1,26 +1,40 @@
 #include "common.h"
 
-#include "dicom.h"
-#include "matrix.h"
+// #include "dicom.h"
+// #include "matrix.h"
 
 /*
-cmake -A Win32 -B build -S . -DCMAKE_BUILD_TYPE=DEBUG -DCMAKE_TOOLCHAIN_FILE=C:\Packages\scripts\buildsystems\vcpkg.cmake
+cmake -A Win32 -B build -S . -DCMAKE_BUILD_TYPE=DEBUG -G "Visual Studio 17 2022" -DCMAKE_TOOLCHAIN_FILE=C:\Packages\scripts\buildsystems\vcpkg.cmake
 && cmake --build .\build\ --config Release
 && cmake --build .\build\
 */
 
-// #include <dcmtk/config/osconfig.h>    /* make sure OS specific configuration is included first */
-// #include <dcmtk/dcmdata/dcfilefo.h>
-// #include <dcmtk/dcmdata/dcmetinf.h>
-// #include <dcmtk/dcmimgle/dcmimage.h>
+#include <dcmtk/config/osconfig.h>    /* make sure OS specific configuration is included first */
+#include <dcmtk/dcmdata/dcfilefo.h>
+#include <dcmtk/dcmdata/dcmetinf.h>
+#include <dcmtk/dcmimgle/dcmimage.h>
+
 
 #include <opencv2/core.hpp>
-#include <opencv2/opencv.hpp>
+#include <opencv2/opencv.hpp> 
 #include <opencv2/imgcodecs.hpp>
 #include <opencv2/highgui.hpp>
 
 auto main(int argc, char const * const argv[]) -> int
 {
+   print("... {} ...\n", __cplusplus);
+
+   auto const ints = {0,1,2,3,4,5};
+   auto even = [](int i) { return 0 == i % 2; };
+   auto square = [](int i) { return i * i; };
+
+    // "pipe" syntax of composing the views:
+    for (int i : ints | std::views::filter(even) | std::views::transform(square)) {
+        std::cout << i << ' ';
+    }
+
+    std::cout << '\n';
+
    const cv::String keys = 
       "{help h usage ? |      | print this message   }"
       "{@source        |<none>| source for process   }"
@@ -61,6 +75,31 @@ auto main(int argc, char const * const argv[]) -> int
 
    // DcmFileFormat format;
    // DcmDataset dataset;
+
+   if (fs::path(sourceFile).extension() == ".dcm")
+   {
+      print("DCM file detected\n");
+      // DicomImage image(sourceFile.c_str());
+
+      // auto frameCount = image.getFrameCount();
+      // auto firstFrame = image.getFirstFrame();
+      // auto frameTime = image.getFrameTime();
+
+      // auto numOfFrames = image.getNumberOfFrames();
+      // auto repoFrame = image.getRepresentativeFrame();
+      // auto dataSize = image.getOutputDataSize();
+
+      // auto d = image.getDepth();
+      // auto h = image.getHeight();
+      // auto w = image.getWidth();
+
+      // print("image: "
+      //    "d {}, h {}, w {}\n"
+      //    "fc {}, ff {}, ft {}\n"
+      //    "nf {}, rf {}, ds {}\n"
+      //    "\n", d, h, w
+      // );
+   }
 
    cv::Mat img = cv::imread(sourceFile, cv::IMREAD_COLOR);
 
