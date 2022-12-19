95945188cad97fdb19e8db97ec826626cf8fd378
JayDalton <github@t-jay.net>
2022-12-19 22:57:35 +0100

    move file contents to right

--- console.h
+++ console.h
@@ -2,7 +2,105 @@
 
 #include "common.h"
 
+// #include "windows.h"
+#include "dicom.h"
+#include "matrix.h"
+
 namespace cmd {
+
+   enum class Category { None, Menu, List, Info, Show };
+
+   using ResultList = std::vector<Category>;
+
+   //////////////////////////////////////////////////////////////////////////////
+
+   struct Repository
+   {
+      auto isStopword(const String& word) const -> bool
+      {
+         return m_stopwords.contains(word);
+      }
+
+      auto findRoot(const String& word) const -> String
+      {
+         if (m_rootword.contains(word))
+         {
+            return m_rootword.at(word);
+         }
+         return word;
+      }
+
+      auto getIntent(const String& word) const -> std::optional<Category>
+      {
+         if (m_meanings.contains(word))
+         {
+            return m_meanings.at(word);
+         }
+         return std::nullopt;
+      }
+
+   private:
+      std::unordered_set<String> m_stopwords{{
+         "me", "an", "a", "in"
+      }};
+
+      std::map<String, String> m_rootword{{
+         // {"home", "menu"}, ???
+         {"view", "show"},
+      }};
+
+      std::map<String, Category> m_meanings{{
+         {"menu", Category::Menu},
+         {"list", Category::List},
+         {"info", Category::Info},
+         {"show", Category::Show},
+      }};
+   };
+
+   auto normalize(StringView input) -> StringList
+   {
+      using namespace std::views;
+
+      auto words = input 
+         | filter([](unsigned char c) { return std::isalpha(c) || std::isspace(c); })
+         | transform([](unsigned char c) { return std::tolower(c); })
+         // | filter/remove consecutive spaces, C++23?
+         | split(' ')
+         | transform([](auto&& c) -> String { return { c.begin(), c.end() }; })
+         | filter([](auto&& c) { return !c.empty(); })
+      ;
+
+      return { words.begin(), words.end() };
+   }
+
+   //////////////////////////////////////////////////////////////////////////////
+
+   struct Recognizer
+   {
+      auto calculate(StringView input) -> ResultList
+      {
+         return exctract(normalize(input));
+      }
+
+      auto exctract(const StringList& words) -> ResultList
+      {
+         using namespace std::views;
+
+         auto intent = words
+            | filter([this](const String& word) { return !m_repo.isStopword(word); })
+            | transform([this](const String& word) { return m_repo.findRoot(word); })
+            | transform([this](const String& word) { return m_repo.getIntent(word); })
+            | filter([](auto&& opt){ return opt.has_value(); })
+            | transform([](auto&& opt){ return opt.value(); })
+         ;
+
+         return { intent.begin(), intent.end() };
+      }
+
+   private:
+      Repository m_repo;
+   };
+
 
    auto createCommand() 
    {
@@ -21,11 +119,17 @@ namespace cmd {
       );
    }
 
-   auto get_file_list(std::string_view folder) -> PathList
+   auto get_file_list(std::string_view source) -> PathList
    {
       using namespace std::ranges::views;
 
-      return fs::directory_iterator{folder}
+      if (!fs::exists(source) || !fs::is_directory(fs::status(source)))
+      {
+         print("source is no directory: {}\n", source);
+         return {};
+      }
+
+      return fs::directory_iterator{source}
          | filter([](auto&& entry){ return entry.exists(); })
          | filter([](auto&& entry){ return entry.is_regular_file(); })
          | filter([](auto&& entry){ return 0 < entry.file_size(); })
@@ -38,7 +142,9 @@ namespace cmd {
    auto show_list(std::string_view folder) -> void
    {
       auto res = get_file_list(folder) 
-         | std::views::transform([](auto&& path){ return path.filename().string(); });
+         | std::views::transform([](auto&& path){ return dcm::read_file_info(path); })
+         | std::views::transform([](auto&& info){ return dcm::format_file_info(info); })
+      ;
 
       std::ranges::copy(res, std::ostream_iterator<String>(std::cout, "\n"));
    }
@@ -68,26 +174,22 @@ namespace cmd {
 
    struct Console
    {
+      Console() { print("-- ctor\n"); }
+
       int run(std::string_view folder)
       {
          print("Contemporary C++ in Action\n");
 
-         show_menu();
+         // show_menu();
          show_list(folder);
 
          while (auto input = readInput())
          {
-            // auto [x,y] = m_engine.find(input.value());
-            // if () 
+            // for (auto item : m_engine.calculate(input.value()))
             // {
-
+            //    print("i ");
             // }
 
-            for (auto c : (*input))
-            {
-               // print("int: {0:d};  hex: {0:#x};  oct: {0:#o};  bin: {0:#b}\n", c);
-               print(L"int: {0:d};  hex: {0:#x};  oct: {0:#o};  bin: {0:#b}\n", c);
-            }
          }
 
          print("End!\n");
@@ -95,7 +197,7 @@ namespace cmd {
       }
 
    private:
-      // engine
+      Recognizer m_engine;
    };
 
 }
--- dicom.h
+++ dicom.h
@@ -1,8 +1,234 @@
 #pragma once
 
+#include "common.h"
+
+#include <dcmtk/config/osconfig.h>    /* make sure OS specific configuration is included first */
+#include <dcmtk/dcmdata/dcfilefo.h>
+#include <dcmtk/dcmdata/dcmetinf.h>
+#include <dcmtk/dcmimgle/dcmimage.h>
+#include <dcmtk/dcmdata/dctk.h>
+#include <dcmtk/dcmdata/dcistrmf.h> 
+#include <dcmtk/dcmdata/dcpxitem.h>
+
 namespace dcm {
 
 
+   // print("directory_iterator:\n");
+   // using namespace std::ranges::views;
 
+   // static std::string_view source{"..."};
+
+   // if (fs::path(source).extension() == ".dcm")
+   // {
+   //    print("DCM file detected\n");
+
+
+   //    /* approach 1 */
+   //    {
+   //       OFCondition status;
+   //       DcmMetaInfo metainfo;
+   //       status = metainfo.loadFile(source.c_str());
+   //       if (status.good())
+   //       {
+   //          OFString sopClassUID, xferUID;
+   //          if (metainfo.findAndGetOFString(DCM_MediaStorageSOPClassUID, sopClassUID).good())
+   //                std::cout << "SOP Class UID: " << sopClassUID << OFendl;
+   //          if (metainfo.findAndGetOFString(DCM_TransferSyntaxUID, xferUID).good())
+   //                std::cout << "Transfer Syntax UID: " << xferUID << OFendl;
+   //          metainfo.print(std::cout);
+   //       }
+   //    }
+
+   //    print("----------------- 1\n");
+
+   //    {
+
+   //       print("FileFormat\n");
+   //       OFCondition result = EC_Normal;
+   //       /* Load file and get pixel data element */
+   //       DcmFileFormat dfile;
+   //       result = dfile.loadFile(source.c_str());
+   //       if (result.bad())
+   //       {
+   //          print("load file\n");
+   //          return 1;
+   //       }
+   //       DcmDataset *data = dfile.getDataset();
+   //       if (data == NULL)
+   //       {
+   //          print("dataset\n");
+   //          return 1;
+   //       }
+   //       DcmElement* element = NULL;
+   //       result = data->findAndGetElement(DCM_PixelData, element);
+   //       if (result.bad() || element == NULL)
+   //       {
+   //          print("GetElement\n");
+   //          return 1;
+   //       }
+   //       DcmPixelData *dpix = NULL;
+   //       dpix = OFstatic_cast(DcmPixelData*, element);
+
+   //       /* Since we have compressed data, we must utilize DcmPixelSequence
+   //          in order to access it in raw format, e. g. for decompressing it
+   //          with an external library.
+   //          */
+   //       DcmPixelSequence *dseq = NULL;
+   //       E_TransferSyntax xferSyntax = EXS_Unknown;
+   //       const DcmRepresentationParameter *rep = NULL;
+   //       // Find the key that is needed to access the right representation of the data within DCMTK
+   //       dpix->getOriginalRepresentationKey(xferSyntax, rep);
+   //       // Access original data representation and get result within pixel sequence
+   //       result = dpix->getEncapsulatedRepresentation(xferSyntax, rep, dseq);
+   //       print("getEncapsulatedRepresentation\n");
+   //       if ( result == EC_Normal )
+   //       {
+   //          print("Encapsulated Representation\n");
+   //          DcmPixelItem* pixitem = NULL;
+   //          // Access first frame (skipping offset table)
+   //          dseq->getItem(pixitem, 1);
+   //          if (pixitem == NULL)
+   //          {
+   //             print("getItem\n");
+   //             return 1;
+   //          }
+   //          Uint8* pixData = NULL;
+   //          // Get the length of this pixel item (i.e. fragment, i.e. most of the time, the lenght of the frame)
+   //          Uint32 length = pixitem->getLength();
+   //          if (length == 0)
+   //          {
+   //             print("getLength\n");
+   //             return 1;
+   //          }
+   //          // Finally, get the compressed data for this pixel item
+   //          result = pixitem->getUint8Array(pixData);
+   //          // Do something useful with pixData...
+   //       }
+   //       else
+   //          print("result bad\n");
+   //    }
+
+   struct FileInfo 
+   {
+      double FrameTime{0};
+      std::uint64_t FrameCount{0};
+      std::uint64_t FirstFrame{0};
+      std::uint64_t DataSize{0};
+      std::uint64_t Height{0};
+      std::uint64_t Width{0};
+      std::uint32_t Depth{0};
+      bool isMonochrome{false};
+      fs::path Path;
+   };
+
+   auto read_file_info(const fs::path& path) -> FileInfo 
+   {
+      auto source = path.filename().string();
+      DicomImage image(source.c_str());
+
+      if (image.getStatus() != EI_Status::EIS_Normal)
+      {
+         return {};
+      }
+
+      FileInfo info;
+      info.FrameCount = image.getFrameCount();
+      info.FirstFrame = image.getFirstFrame();
+      info.FrameTime = image.getFrameTime();
+
+      // info.numOfFrames = image.getNumberOfFrames();
+      // info.repoFrame = image.getRepresentativeFrame();
+      info.DataSize = image.getOutputDataSize();
+
+      info.Depth = image.getDepth();
+      info.Height = image.getHeight();
+      info.Width = image.getWidth();
+      info.Path = path;
+
+      info.isMonochrome = image.isMonochrome();
+
+      return info;
+   }
+
+   auto format_file_info(const FileInfo& info) -> String 
+   {
+      return std::format("{}: {}x{} | {}-{} | {}_{}_{}_{}"
+         , info.Path.filename().string()
+         , info.Height
+         , info.Width
+         , info.Depth
+         , info.isMonochrome
+         , info.FrameCount
+         , info.FirstFrame
+         , info.FrameTime
+         , info.DataSize
+      );
+   }
+
+
+   //    print("----------------- 2\n");
+
+   //    {
+   //       DicomImage image(source.c_str());
+
+   //       auto frameCount = image.getFrameCount();
+   //       auto firstFrame = image.getFirstFrame();
+   //       auto frameTime = image.getFrameTime();
+
+   //       auto numOfFrames = image.getNumberOfFrames();
+   //       auto repoFrame = image.getRepresentativeFrame();
+   //       auto dataSize = image.getOutputDataSize();
+
+   //       auto d = image.getDepth();
+   //       auto h = image.getHeight();
+   //       auto w = image.getWidth();
+
+   //       print("image: "
+   //          "d {}, h {}, w {}\n"
+   //          "fc {}, ff {}, ft {}\n"
+   //          "nf {}, rf {}, ds {}\n"
+   //          "\n", d, h, w, 
+   //          frameCount, firstFrame, frameTime,
+   //          numOfFrames, repoFrame, dataSize         
+   //       );
+
+   //       const int m_depth{8};
+   //       const auto status{image.getStatus()};
+   //       if (status != EIS_Normal) { print("dicom image status\n"); }
+
+   //       const std::unordered_map<int, std::pair<int, int>> formats {
+   //          {8, {CV_8UC1, CV_8UC3}},
+   //          {16, {CV_16UC1, CV_16UC3}},
+   //       };
+
+   //       auto isMono {image.isMonochrome()};
+   //       auto format {formats.at(m_depth)};
+   //       auto type {isMono ? format.first : format.second};
+   //       auto dims {isMono ? 1 : 3};
+
+   //       auto width {static_cast<int>(image.getWidth())};
+   //       auto height {static_cast<int>(image.getHeight())};
+   //       auto length {static_cast<int>(image.getOutputDataSize(m_depth))};
+   //       auto buffer {image.getOutputData(m_depth, 0)}; // immer nur ein Frame drin!
+   //       // dpassert(buffer && (length == width * height * dims));
+
+   //       auto matrix {cv::Mat(height, width, type, const_cast<void*>(buffer)).clone()}; // copy
+
+   //       print("matrix: "
+   //          "chans {}, depth {}\n"
+   //          "cols {}, rows {}\n"
+   //          "dims {} flags {}\n",
+   //          // "step {}, size {}\n",
+   //          matrix.channels(), matrix.depth(),
+   //          matrix.cols, matrix.rows, 
+   //          matrix.dims, matrix.flags
+   //          // matrix.step, matrix.size
+   //       );
+
+   //       print("...\n");
+   //       cv::imshow("Display window", matrix);
+   //       int k = cv::waitKey(0); // Wait for a keystroke in the window
+   //    }
+   // }
    
 }
