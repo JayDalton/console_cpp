8b3e17937367a2a311a7b1a456ee197c302d96a0
gerlaju <juergen_thomas.gerlach@mercedes-benz.com>
2023-03-04 13:07:25 +0100

    test some python commands

--- analyze.py
+++ analyze.py
@@ -1,11 +1,16 @@
 import cv2
+import matplotlib.pyplot as plt
 import numpy as np
 import pydicom
 
+x = np.arange(0, 5, 0.1)
+y = np.sin(x)
+plt.plot(x, y)
+
 ds = pydicom.dcmread('assets/PE_Image.dcm')
+print(ds)
 
 image_2d = ds.pixel_array.astype(float)
-
 print(image_2d)
 
 image_2d_scaled = (np.maximum(image_2d, 0) / image_2d.max()) * 255.0
