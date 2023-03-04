7f642322701180eb2396439eb421998c723928bd
Jay Desktop <github@t-jay.net>
2023-03-04 12:04:08 +0100

    add python file

---
+++ analyze.py
@@ -0,0 +1,29 @@
+import cv2
+import numpy as np
+import pydicom
+
+ds = pydicom.dcmread('assets/PE_Image.dcm')
+
+image_2d = ds.pixel_array.astype(float)
+
+print(image_2d)
+
+image_2d_scaled = (np.maximum(image_2d, 0) / image_2d.max()) * 255.0
+image_2d_scaled = np.uint8(image_2d_scaled)
+
+# Creating GUI window to display an image on screen
+# first Parameter is windows title (should be in string format)
+# Second Parameter is image array
+cv2.imshow("image", image_2d_scaled)
+ 
+# To hold the window on screen, we use cv2.waitKey method
+# Once it detected the close input, it will release the control
+# To the next line
+# First Parameter is for holding screen for specified milliseconds
+# It should be positive integer. If 0 pass an parameter, then it will
+# hold the screen until user close it.
+cv2.waitKey(0)
+ 
+# It is for removing/deleting created GUI window from screen
+# and memory
+cv2.destroyAllWindows()
